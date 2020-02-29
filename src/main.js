89acd78957d857e7889ad2ec5ad7d343665ef774
888 <songych@163.com>
2020-02-29 23:42:51 +0800

    video

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 function setFontSize() {
-    const fullWidth = document.body.clientWidth;
+    let fullWidth = document.body.clientWidth;
+    if(fullWidth>1948) {
+        fullWidth = 1948;
+    }else if(fullWidth<1536) {
+        fullWidth = 1536;
+    }
     var html = document.getElementsByTagName('html')[0];
     html.style.fontSize = fullWidth / 20 + 'px'
 }
