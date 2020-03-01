8a7873052fdb262c024237df8784116485634d0b
888 <songych@163.com>
2020-03-01 22:54:44 +0800

    服务优势

--- src/js/rem.js
+++ src/js/rem.js
@@ -3,7 +3,7 @@ function setFontSize() {
     if(fullWidth>1948) {
         fullWidth = 1948;
     }else if(fullWidth<1536) {
-        fullWidth = 1536;
+        // fullWidth = 1536;
     }
     var html = document.getElementsByTagName('html')[0];
     html.style.fontSize = fullWidth / 20 + 'px'
